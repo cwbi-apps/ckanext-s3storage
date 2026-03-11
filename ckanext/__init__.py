8ff3f550b3a69391ebb9aed748af6380c04e4f94
bmulcahy <brian.p.mulcahy@erdc.dren.mil>
2026-03-11 13:09:12 -0400

    adfs

---
+++ ckanext/__init__.py
@@ -0,0 +1,1 @@
+# This file marks the 'ckanext' directory as a package
--- setup.py
+++ setup.py
@@ -4,15 +4,12 @@ setup(
     name="ckanext-s3storage",
     version="0.0.1",
     url="https://github.com/cwbi-apps/ckanext-s3storage",
-    packages=find_packages(
-        include=["ckanext", "ckanext.*"], 
-        exclude=["ez_setup", "examples", "tests"]
-    ),
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "boto3>=1.28",
         "rq",
-        "redis"
+        "redis",
     ],
     entry_points="""
         [ckan.plugins]
