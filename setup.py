50818a42ffebf55508517e86f1266f87560570ca
bmulcahy <brian.p.mulcahy@erdc.dren.mil>
2026-03-11 13:11:40 -0400

    i don't know anymore

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
+version = "0.0.1"
+
 setup(
     name="ckanext-s3storage",
-    version="0.0.1",
+    version=version,
+    description="CKAN S3 resource storage backend",
     url="https://github.com/cwbi-apps/ckanext-s3storage",
+    author="CWBI",
+    license="MIT",
     packages=find_packages(),
+    namespace_packages=["ckanext"],
     include_package_data=True,
+    zip_safe=False,
     install_requires=[
         "boto3>=1.28",
         "rq",
-        "redis",
+        "redis"
     ],
     entry_points="""
         [ckan.plugins]
