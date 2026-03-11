bf80d5911656441721952589d3bc35dd60eb314c
bmulcahy <brian.p.mulcahy@erdc.dren.mil>
2026-03-11 12:02:44 -0400

    Updated setup after error

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="ckanext-s3storage",
-    version="0.4",
-    packages=find_packages(),
+    version="0.0.1",
+    url='https://github.com/cwbi-apps/ckanext-s3storage',
+    packages=find_packages(include=["ckanext", "ckanext.*"],exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=["ckanext"],
+    include_package_data=True,
     install_requires=[
         "boto3>=1.28",
         "rq",
